0d4d499f37edf273527e6534d0f032e8445b1866
Galyna Popova <galka_po@mail.ru>
2018-05-02 21:42:22 +0300

    testTask-draftLayout

---
+++ TestShop/src/js/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// Slider OwlCarousel
+
+$(document).ready(function(){
+    $(".slide-one").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        navSpeed: 1000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:true
+            },
+            992:{
+                items:1,
+                nav:true,
+                loop:true
+            }
+        }
+    });
+    $(".slide-two").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        navSpeed: 1000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            480:{
+                items:1,
+                nav:true
+            },
+            768:{
+                items:2,
+                nav:true
+            },
+            992:{
+                items:3,
+                nav:true,
+                loop:true
+            }
+        }
+    });
+});
+
+
