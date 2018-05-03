2023bf9ebf82503a64125ece74748a3b05c1d3b1
Galyna Popova <galka_po@mail.ru>
2018-05-03 14:02:47 +0300

    add [products,reviews]

--- TestShop/js/script.min.js
+++ TestShop/js/script.min.js
@@ -51,4 +51,33 @@ $(document).ready(function(){
     });
 });
 
+// API Get products, reviews
 
+var $link = 'http://demo4072172.mockable.io/api';
+$(document).ready(function() {
+    $.get( $link+'/product/1', function(response) {
+        $('#products-tpl').tmpl(response).appendTo('#product1');
+    });
+    $.get($link+'/product/2', function(response) {
+        $('#products-tpl').tmpl(response).appendTo('#product2');
+    });
+});
+
+$('.product1').one('click', function(){
+    $.get($link+'/reviews/1', function(response) {
+        $('#description1-tmpl').tmpl(response).appendTo('#description1');
+    });
+});
+$('.product1').on('click', function(e){
+    e.preventDefault();
+    $('.modal1').toggleClass('active');
+});
+$('.product2').one('click', function(){
+    $.get($link+'/reviews/2', function(response) {
+        $('#description2-tmpl').tmpl(response).appendTo('#description2');
+    });
+});
+$('.product2').on('click', function(e){
+    e.preventDefault();
+    $('.modal2').toggleClass('active');
+});
