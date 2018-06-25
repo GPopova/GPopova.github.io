ad34c7eb4cf3a1471a55cb5511c8639f14027e30
Galyna Popova <galka_po@mail.ru>
2018-06-25 23:08:42 +0300

    add generic page

--- Lawyer/js/script.js
+++ Lawyer/js/script.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-  $(".fancybox").fancybox({
-    'hideOnContentClick': true
-  });
-
-  $('.top-menu ul').slicknav({
-    prependTo: '.menu-responsive',
-    duration: 100
-  });
 
   $('.data-menu li a').click(function () {
     event.preventDefault();
@@ -20,4 +12,49 @@ $(document).ready(function () {
 
     }
   });
+
+
+  $(".fancybox").fancybox({
+    'hideOnContentClick': true,
+    afterShow: function (a, z) {
+      initMap()
+    }
+  });
+
+  $('.top-menu ul').slicknav({
+    prependTo: '.menu-responsive',
+    duration: 100
+  });
+
+  if(window.innerWidth < 767){
+    function myfun() {
+      var width = parseInt($('.top-line .top-right').css('width'));
+
+      $(".slicknav_nav").css('width', width);
+    };
+
+    $( window ).resize(myfun);
+    $( document ).ready(myfun);
+  }
+
+
+  $('.counties-li li:gt(13)').hide();
+  $('.counties-li li.more a').click(function() {
+    event.preventDefault();
+    $('.counties-li li.more').addClass('display-none');
+    $('.counties-li li:gt(13)').show();
+
+  });
+
+  $('.counties-li li.hide a').click(function() {
+    event.preventDefault();
+    $('.counties-li li.hide').addClass('display-none');
+    $('.counties-li li.more').removeClass('display-none');
+    $('.counties-li li:gt(13)').hide();
+  });
+
+
+  $('select').niceSelect();
+
+
 });
