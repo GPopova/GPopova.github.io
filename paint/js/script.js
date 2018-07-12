d1baba0bc03a84b87871de65c6fc789f4ef0ae5d
Galyna Popova <galka_po@mail.ru>
2018-07-12 22:32:03 +0300

    add

--- paint/js/script.js
+++ paint/js/script.js
@@ -11,6 +11,21 @@ $('#openCatalog').on('click', function () {
    $('.catalogInner').toggleClass('open');
 });
 
+// button
+
+$('.check').on('click', function () {
+    $(this).toggleClass('checked');
+});
+
+// Menu City
+
+var a = $('.select').find('a');
+
+a.on('click', function (){
+    $('#selectCity').toggleClass('open');
+});
+
+
 
 // Slider OwlCarousel
 
@@ -40,4 +55,63 @@ $(document).ready(function(){
         }
     });
 });
+
+
+$(document).ready(function(){
+    $(".slider2").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        URLhashListener:true,
+        startPosition: 'URLHash',
+        navSpeed: 1000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:true,
+            },
+            992:{
+                items:2,
+                nav:true,
+                loop:true
+
+            }
+        }
+    });
+});
+
+
+$(document).ready(function(){
+    $(".slider3").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        URLhashListener:true,
+        startPosition: 'URLHash',
+        infinite: false,
+        navSpeed: 1000,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:true,
+            },
+            992:{
+                items:1,
+                nav:true,
+                loop:true
+
+            }
+        }
+    });
+});
+
+//
 
